69cc757e617cd65b727660c9ad3dee553de7eb47
j11024335-beep <j11024335@gmail.com>
2026-05-29 17:23:40 -0300

    Update Firebase configuration with project details

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 // Configuração do Firebase
 // IMPORTANTE: Substitua as configurações abaixo pelas do seu projeto no Firebase Console
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAW37sIiJ8WnJNjx8of2klOyToPeh0Bjfk",
+  authDomain: "cardapio-bcf38.firebaseapp.com",
+  databaseURL: "https://cardapio-bcf38-default-rtdb.firebaseio.com",
+  projectId: "cardapio-bcf38",
+  storageBucket: "cardapio-bcf38.firebasestorage.app",
+  messagingSenderId: "991711198204",
+  appId: "1:991711198204:web:ba21d974f5d4d0a21c5079",
+  measurementId: "G-QQKN182X35"
 };
-
 // Inicializar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, doc, setDoc, updateDoc, deleteDoc, onSnapshot, query, where, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
